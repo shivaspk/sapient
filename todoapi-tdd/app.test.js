cd9bf16b0fc1fc6f5981be64a418e81845cf3ca2
shivaspk <shivaspk.siva@gmail.com>
2021-08-28 12:30:49 +0530

    updated with test cases

--- todoapi-tdd/app.test.js
+++ todoapi-tdd/app.test.js
@@ -22,10 +22,60 @@ describe('Todos API', () =>{
             )
         })
     })
-    it('GET /todos/id ===> Todo by ID', () => {})
-    it('GET /todos/id ===> 404', () => {})
-    it('POST /todos ===> Todo', () => {})
-    it('POST /todos ===> array of Todos', () => {})
+    it('GET /todos/id ===> Todo by ID', () => {
+        return request(app)
+        .get('/todos/1')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+            expect(response.body).toEqual(
+                expect.objectContaining(
+                    {
+                        id: 1,
+                        name: expect.any(String),
+                        done: any(Boolean)
+                    }
+                )
+            )
+        })
+    })
+    it('GET /todos/id ===> 404', () => {
+        return request(app)
+        .get('/todos/555555')
+       // .expect('Content-Type', /json/)
+        .expect(404)
+    })
+    it('POST /todos ===> Todo', () => {
+        return request(app)
+        .post('/todos')
+        .send({
+            name: 'my todo',
+            done: false        
+        })
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .then((response) => {
+            expect(response.body).toEqual(
+                expect.objectContaining(
+                    {
+                        id: 1,
+                        name: 'my todo',
+                        done: false
+                    }
+                )
+            )
+        })
+
+    })
+    it('POST /todos ===> Validate Body of Todo', () => {
+        return request(app)
+        .post('/todos')
+        .send({
+            name: 'my todo',
+            done: false        
+        })
+        .expect(400)
+    })
 })
 
 //GET /todos
